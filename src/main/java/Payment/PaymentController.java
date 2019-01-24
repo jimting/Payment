9b02f2e1b36212c1a41c58f39c03281980c0ab6b
jimting <handsome841206@gmail.com>
2019-01-24 19:56:28 +0800

    type 0.0.1

---
+++ src/main/java/Payment/PaymentController.java
@@ -0,0 +1,35 @@
+package Payment;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+
+
+
+@RestController
+public class PaymentController {
+	
+	@CrossOrigin(origins = "*")
+	@RequestMapping("/")
+    public String index() 
+    {
+		return "success";
+    }
+	
+	@CrossOrigin(origins = "*")
+	@RequestMapping(value = "Payment", method = RequestMethod.GET)
+    public String getGrocery()
+    {
+    	return "success";
+    }
+	
+	
+}
+
+
+
---
+++ src/test/java/GroceryInventory/GroceryInventoryTest.java
@@ -0,0 +1,15 @@
+package GroceryInventory;
+
+import org.junit.Test;
+
+public class GroceryInventoryTest {
+
+	@Test
+	public void testDatabase()
+	{
+		//測試是否成功連線到DB
+		//if(GroceryInventory.getGrocery(userID))System.out.println("答對了！");
+		//else	System.out.println("正確答案應該是5");
+	}
+
+}
