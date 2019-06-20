d498f836f9d28c52a879f37c11cdc9cc3f8433b8
i11007020 <i11007020@gmail.com>
2019-06-20 14:48:56 +0800

    add Feign client

--- src/main/java/Payment/PaymentController.java
+++ src/main/java/Payment/PaymentController.java
@@ -1,14 +1,13 @@
 package Payment;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -16,6 +15,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Api(value = "PaymentController", tags = "與付錢相關的所有一切都在這裡")
 @RestController
 public class PaymentController {
+	@Autowired
+	FeignInterface feignInterface;
+	
 	
 	@ApiOperation(value = "測試此伺服器是否成功連線", notes = "成功連線就回傳success")
 	@CrossOrigin(origins = "*")
