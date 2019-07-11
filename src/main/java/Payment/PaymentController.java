db919a359851e87823b99238304bb9fd3b9c1c6d
Chen <i11007020@gmail.com>
2019-07-11 21:39:32 +0800

    delete feignInterface

--- src/main/java/Payment/PaymentController.java
+++ src/main/java/Payment/PaymentController.java
@@ -1,13 +1,11 @@
 package Payment;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 
 
 
@@ -15,8 +13,6 @@ import io.swagger.annotations.ApiOperation;
 @Api(value = "PaymentController", tags = "與付錢相關的所有一切都在這裡")
 @RestController
 public class PaymentController {
-	@Autowired
-	FeignInterface feignInterface;
 	
 	
 	@ApiOperation(value = "測試此伺服器是否成功連線", notes = "成功連線就回傳success")
@@ -29,7 +25,7 @@ public class PaymentController {
 	
 	@ApiOperation(value = "購買物品", notes = "成功購買就回傳success")
 	@CrossOrigin(origins = "*")
-	@RequestMapping(value = "payment", method = RequestMethod.GET)
+	@RequestMapping(value = "/payment", method = RequestMethod.GET)
     public String payment()
     {
     	return "success";
