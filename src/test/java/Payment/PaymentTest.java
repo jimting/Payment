94fa0c52532b080df5013043bfd1e50ac0659385
Chen <i11007020@gmail.com>
2019-07-12 18:41:22 +0800

    update GroventoryTest to PaymentTest

---
+++ src/test/java/Payment/PaymentTest.java
@@ -0,0 +1,20 @@
+package Payment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PaymentTest {
+
+    @Test
+    public void testDatabase()
+    {
+        //測試是否成功連線到DB
+        //if(GroceryInventory.getGrocery(userID))System.out.println("答對了！");
+        //else	System.out.println("正確答案應該是5");
+    }
+
+}
