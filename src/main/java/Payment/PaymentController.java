4a6832cb73d691766e6ad2365a5acf0d8e65da61
Chen <i11007020@gmail.com>
2019-07-14 15:55:14 +0800

    add arguments of payment

--- src/main/java/Payment/PaymentController.java
+++ src/main/java/Payment/PaymentController.java
@@ -2,12 +2,8 @@ package Payment;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-
-
+import io.swagger.annotations.ApiParam;
+import org.springframework.web.bind.annotation.*;
 
 
 @Api(value = "PaymentController", tags = "與付錢相關的所有一切都在這裡")
@@ -26,7 +22,7 @@ public class PaymentController {
 	@ApiOperation(value = "購買物品", notes = "成功購買就回傳success")
 	@CrossOrigin(origins = "*")
 	@RequestMapping(value = "payment", method = RequestMethod.GET)
-    public String payment()
+    public String payment(@ApiParam(required = true, name = "userID", value = "使用者ID")@RequestParam("userID") String userID, @ApiParam(required = true, name = "price", value = "付款金額")@RequestParam("price") String price)
     {
     	return "success";
     }
