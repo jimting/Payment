492cca20f5d055db92a6e8324373cae52cdecda3
Chen <i11007020@gmail.com>
2019-07-14 16:32:39 +0800

    add test

--- src/test/java/Payment/PaymentTest.java
+++ src/test/java/Payment/PaymentTest.java
@@ -2,8 +2,11 @@ package Payment;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -15,6 +18,21 @@ public class PaymentTest {
         //測試是否成功連線到DB
         //if(GroceryInventory.getGrocery(userID))System.out.println("答對了！");
         //else	System.out.println("正確答案應該是5");
+    }
+
+    @Autowired
+    PaymentController paymentController;
+
+    @Test
+    public void testIndex()
+    {
+        assertEquals( paymentController.index(), "success");
+    }
+
+    @Test
+    public void testPayment()
+    {
+        assertEquals( paymentController.payment("1","250"), "success");
     }
 
 }
